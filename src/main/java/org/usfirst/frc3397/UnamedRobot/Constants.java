5471195d3d1fed1e12177898f762880386ec4c94
Robolions <robolions3397@gmail.com>
2019-02-14 21:00:14 -0600

    first commit

---
+++ src/main/java/org/usfirst/frc3397/UnamedRobot/Constants.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc3397.UnamedRobot;
+
+/**
+ * Add your docs here.
+ */
+public class Constants {
+    public static int backRightMotorPWM = 0;
+    public static int backLeftMotorPWM = 3;
+    public static int frontRightMotorPWM = 1;
+    public static int frontLeftMotorPWM = 2;
+    public static int HatchArmPWM = 5;
+    public static int intakeLiftPWM = 4;
+    public static int leftIntakePWM = 6;
+    public static int rightIntakePWM = 7;
+
+    public static int SolenoidPCM = 0; 
+
+    // DIO is for off and on switches
+    public static int liftBackStopDIO = 0;
+    
+    // XBOX Controller
+    
+
+    // Fight Stick
+    public static int FightStickX = 3;
+    public static int FightStickY = 4;
+    public static int FightStickA = 1;
+    public static int FightStickB = 2;
+    public static int FightStickL1 = 5;
+    public static int FightStickR1 = 6;
+}
